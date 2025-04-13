0f2d980fd789eba7aebdef606f26364f7f531169
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-13 15:54:07 +0530

    Add Subtraction functionality

---
+++ Subtraction.java
@@ -0,0 +1,6 @@
+public class Subtraction {
+    // Perform subtraction of two numbers
+    public static void perform(double a, double b) {
+        System.out.println("Result: " + (a - b));
+    }
+}
