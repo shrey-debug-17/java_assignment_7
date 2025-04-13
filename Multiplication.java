dea5ec3aaeabe699f7108ab1676df4a770c2137c
shrey-debug-17 <ardeshanashrey17@gmail.com>
2025-04-13 15:55:48 +0530

    Add Multiplication Functionality

---
+++ Multiplication.java
@@ -0,0 +1,6 @@
+public class Multiplication {
+    // Perform multiplication of two numbers
+    public static void perform(double a, double b) {
+        System.out.println("Result: " + (a * b));
+    }
+}
